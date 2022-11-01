865834cdf115f49c5a02a291186fb18e57f302ed
formaro <formaro99@gmail.com>
2022-11-02 02:40:31 +0900

    홍마로/EDIT import test

--- src/redux/module/PinSlice.js
+++ src/redux/module/PinSlice.js
@@ -1,10 +1,10 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios'
-
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  pins: [],  // /api/posts/all
-  pin: {     // /api/posts/{postId}
+  pins: [], // /api/posts/all
+  pin: {
+    // /api/posts/{postId}
     postId: 1,
     title: '',
     content: '',
@@ -12,135 +12,140 @@ const initialState = {
     uniqueName: '',
     userId: '',
     profileimage: '',
-    filePath: ''
+    filePath: '',
   },
   isLoading: false,
-  error: null
-}
+  error: null,
+};
 
 // GET api/posts/all : 민주
 export const __getPins = createAsyncThunk(
   'pin/getPins',
   async (payload, thunkAPI) => {
     try {
-      return thunkAPI.fulfillWithValue()
+      return thunkAPI.fulfillWithValue();
     } catch (error) {
-      return thunkAPI.rejectWithValue(error)
+      return thunkAPI.rejectWithValue(error);
     }
   }
-)
+);
 
 // GET api/posts/{postId}
 export const __getPin = createAsyncThunk(
   'pin/getPin',
   async (payload, thunkAPI) => {
     try {
-      return thunkAPI.fulfillWithValue()
+      return thunkAPI.fulfillWithValue();
     } catch (error) {
-      return thunkAPI.rejectWithValue(error)
+      return thunkAPI.rejectWithValue(error);
     }
   }
-)
+);
 
 // PUT api/posts/{postId}
 export const __putPin = createAsyncThunk(
   'pin/putPin',
   async (payload, thunkAPI) => {
     try {
-      return thunkAPI.fulfillWithValue()
+      return thunkAPI.fulfillWithValue();
     } catch (error) {
-      return thunkAPI.rejectWithValue(error)
+      return thunkAPI.rejectWithValue(error);
     }
   }
-)
+);
 
 // DELETE api/posts/{postId}
 export const __deletePin = createAsyncThunk(
   'pin/deletePin',
   async (payload, thunkAPI) => {
     try {
-      return thunkAPI.fulfillWithValue()
+      return thunkAPI.fulfillWithValue();
     } catch (error) {
-      return thunkAPI.rejectWithValue(error)
-    }
-  }
-)
-
-// POST api/posts 
-export const __writePin = createAsyncThunk(
-  'pin/writePin',
-  async (payload, thunkAPI) => {
-    try {
-      return thunkAPI.fulfillWithValue()
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error)
-    }
-  }
-)
-
-const pinSlice = createSlice(
-  {
-    name: 'pin',
-    initialState: initialState,
-    reducers: {},
-    extraReducers: {
-      // getPins
-      [__getPins.pending]: (state, action) => {
-        state.isLoading = true;
-      },
-      [__getPins.fulfilled]: (state, action) => {
-        state.isLoading = false;
-      },
-      [__getPins.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
-      // getPin
-      [__getPin.pending]: (state, action) => {
-        state.isLoading = true;
-      },
-      [__getPin.fulfilled]: (state, action) => {
-        state.isLoading = false;
-      },
-      [__getPin.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
-      // putPin
-      [__putPin.pending]: (state, action) => {
-        state.isLoading = true;
-      },
-      [__putPin.fulfilled]: (state, action) => {
-        state.isLoading = false;
-      },
-      [__putPin.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
-      // deletePin
-      [__deletePin.pending]: (state, action) => {
-        state.isLoading = true;
-      },
-      [__deletePin.fulfilled]: (state, action) => {
-        state.isLoading = false;
-      },
-      [__deletePin.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
-      // writePin
-      [__writePin.pending]: (state, action) => {
-        state.isLoading = true;
-      },
-      [__writePin.fulfilled]: (state, action) => {
-        state.isLoading = false;
-      },
-      [__writePin.rejected]: (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      },
+      return thunkAPI.rejectWithValue(error);
     }
   }
 );
 
-export default pinSlice.reducer
+// POST api/posts
+export const __writePin = createAsyncThunk(
+  'pin/writePin',
+  async (payload, thunkAPI) => {
+    console.log(payload);
+    try {
+      const { data } = await axios.post(
+        'https://week3-board.herokuapp.com/posts',
+        payload
+      );
+      console.log(data);
+      return thunkAPI.fulfillWithValue(data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const pinSlice = createSlice({
+  name: 'pin',
+  initialState: initialState,
+  reducers: {},
+  extraReducers: {
+    // getPins
+    [__getPins.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__getPins.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__getPins.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    // getPin
+    [__getPin.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__getPin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__getPin.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    // putPin
+    [__putPin.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__putPin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__putPin.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    // deletePin
+    [__deletePin.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__deletePin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+    },
+    [__deletePin.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    // writePin
+    [__writePin.pending]: (state, action) => {
+      state.isLoading = true;
+    },
+    [__writePin.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.posts.push(action.payload);
+    },
+    [__writePin.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export default pinSlice.reducer;
