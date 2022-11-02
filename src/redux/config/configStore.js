ff4bd3971aedb97ea37797df90927aaf444be984
mingki831 <mingki831@naver.com>
2022-11-02 18:50:36 +0900

    김정은/ADD configStore에 user 추가
    configStore에 user 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,9 +1,11 @@
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import PinSlice from '../module/PinSlice';
+import user from '../module/LoginSlice'
 
 const store = configureStore({
   reducer: {
     PinSlice,
+    user
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
