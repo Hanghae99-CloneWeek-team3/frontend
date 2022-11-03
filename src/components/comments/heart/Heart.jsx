d2e591246c84f315ac65280b60ad142eb9069c67
Kim Minjoo <roses16.dev@gmail.com>
2022-11-03 14:02:01 +0900

    김민주/ADD : comment

--- src/components/comments/heart/Heart.jsx
+++ src/components/comments/heart/Heart.jsx
@@ -1,20 +1,30 @@
 // components
 import DefaultIcon from "../../defaulticon/DefaultIcon";
-
+import instance from "../../../shared/Request";
+import { useState } from 'react';
 // style
 import { DivHeartBox, DivHeart, SpanNumberHeart } from "./style";
 
-export default function Heart({liked=false, numberLike}){
+export default function Heart({ commentId, liked=false, numberLike}){
+  const [ isLiked, setIsLiked ] = useState(liked);
+  const [ countLike, setCountLike] = useState(numberLike)
 
   // function
   function onClickHeart(){
-    // need to : Heart toggle
-    // API : /api/comments/likes/{commentId} Delete & Post
+    if(isLiked){
+      instance.delete(`api/comments/likes/${commentId}`)
+      setIsLiked(false)
+      setCountLike(countLike - 1)
+    } else {
+      instance.post(`api/comments/likes/${commentId}`)
+      setIsLiked(true)
+      setCountLike(countLike + 1)
+    } 
   }
 
   // render
   return <DivHeartBox>
-    <DefaultIcon size='24'><DivHeart liked={liked}>❤</DivHeart></DefaultIcon>
-    <SpanNumberHeart>{numberLike ? numberLike : ''}</SpanNumberHeart>
+    <DefaultIcon size='24'><DivHeart liked={isLiked} onClick={onClickHeart}>❤</DivHeart></DefaultIcon>
+    <SpanNumberHeart>{countLike ? countLike : ''}</SpanNumberHeart>
   </DivHeartBox>
 }
